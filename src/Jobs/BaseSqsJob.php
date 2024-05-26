31b06c968a1aedd9b53815fdaf8fda06df88bd42
WouterLagerwerf <43637154+WouterLagerwerf@users.noreply.github.com>
2024-05-26 12:08:13 +0200

    Made the Job comply with its parent
    The job was throwing errors that we were not implementing the abstract functions so added those to comply with the base model

--- src/Jobs/BaseSqsJob.php
+++ src/Jobs/BaseSqsJob.php
@@ -7,6 +7,16 @@ use Illuminate\Queue\Jobs\JobName;
 
 class BaseSqsJob extends BaseJob
 {
+    protected $job;
+
+    public function __construct($container, $sqs, $queue, $job)
+    {
+        $this->container = $container;
+        $this->sqs = $sqs;
+        $this->queue = $queue;
+        $this->job = $job;
+    }
+
     /**
      * Fire the job.
      * change the fire method to call the handle method of the job class to accept the format of SNS messages instead of SQS messages.
@@ -14,20 +24,20 @@ class BaseSqsJob extends BaseJob
      */
     public function fire()
     {
-        $payload = $this->formatPayload($this->payload())['job'];
+        $payload = $this->formatPayload($this->payload());
         [$class, $method] = JobName::parse($payload['job']);
 
         ($this->instance = $this->resolve($class))->{$method}($this, $payload['data']);
     }
 
-     /**
+    /**
      * Save the failed job.
      * change the failed method to call the handle method of the job class to accept the format of SNS messages instead of SQS messages.
      * @return void
      */
     protected function failed($e)
     {
-        $payload = $this->formatPayload($this->payload())['job'];
+        $payload = $this->formatPayload($this->payload());
         [$class, $method] = JobName::parse($payload['job']);
 
         if (method_exists($this->instance = $this->resolve($class), 'failed')) {
@@ -42,8 +52,17 @@ class BaseSqsJob extends BaseJob
 
     private function formatPayload($payload)
     {
-        $payload = $this->payload();
-        $message = $this->payload()['Message'];
+        $message = $payload['Message'];
         return json_decode($message, true);
+    }
+
+    public function getJobId()
+    {
+        return $this->job['MessageId'];
+    }
+
+    public function getRawBody()
+    {
+        return json_encode($this->job);
     }
 }
